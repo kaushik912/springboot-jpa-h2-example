ace495eb13cbfb1737f350845f01ca20db1a0ff1
kaushik <kaushik.1986@gmail.com>
2025-01-27 13:17:29 +0530

    added files for spring jpa h2

---
+++ src/main/java/com/example/demo/entity/Department.java
@@ -0,0 +1,31 @@
+// Java Program to Demonstrate Department File
+
+// Importing required package modules
+package com.example.demo.entity;
+
+// Importing required classes (using jakarta.persistence now)
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+// Class
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+}
---
+++ src/main/java/com/example/demo/service/DepartmentService.java
@@ -0,0 +1,24 @@
+// Java Program to Demonstrate DepartmentService File
+
+// Importing required package modules
+package com.example.demo.service;
+import com.example.demo.entity.Department;
+// Importing required classes
+import java.util.List;
+
+// Interface
+public interface DepartmentService {
+
+    // Save operation
+    Department saveDepartment(Department department);
+
+    // Read operation
+    List<Department> fetchDepartmentList();
+
+    // Update operation
+    Department updateDepartment(Department department,
+                                Long departmentId);
+
+    // Delete operation
+    void deleteDepartmentById(Long departmentId);
+}
